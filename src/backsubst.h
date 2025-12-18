daf51331e6e7c5fa840d42f9c7765df5758bc7b3
julrombel-sys <jul.rombel@gmail.com>
2025-12-18 21:21:21 +0100

    Update backsubst.h

--- src/backsubst.h
+++ src/backsubst.h
@@ -1,7 +1,1 @@
-#ifndef BACKSUBST_H
-#define BACKSUBST_H
 
-/*A- macierz gornotrojkatna, b- wektor wyrazow wolnych x- wektor rozwiazan k-rozmiar */
-int backstubs(double A[][100],double b[],double x[],int k);
-
-#endif
