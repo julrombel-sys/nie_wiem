47e3a5b6c3167ab08a588559119f71850f0106c0
julrombel-sys <jul.rombel@gmail.com>
2025-12-18 21:47:15 +0100

    Revert "jtm"

--- src/backsubst.c
+++ src/backsubst.c
@@ -1,1 +1,26 @@
+#include "backsubst.h"
+#include <stdio.h>
+
+int backsubst(double A[][100], double b[], double x[], int k)
+{
+  int i,j;
+  
+  for(i = k - 1; i >= 0; i--)
+  {
+    /*sprawdzanie na przekatnej*/
+    if(A[i][i] == 0.0)
+    {
+      return 1; /*blad*/
+    }
+    x[i] = b[i];
+    for(j = i + 1; j < k; j++)
+      {
+      x[i] -= A[i][j] * x[j];
+      }
+    x[i] /= A[i][i];
+  }
+
+  return 0; /*sukces*/
+}
+
 
--- src/gauss.h
+++
@@ -1,6 +0,0 @@
-#ifndef _GAUSS_H__
-#define _GAUSS_H__
-
-//TODO
-
-#endif //_GAUSS_H__
